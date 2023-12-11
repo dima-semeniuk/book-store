634f23c48a41988ee7305592f844722b9352e8b5
Dima Semeniuk <semeniuk.dima@ukr.net>
2023-12-11 19:08:38 +0200

    added pagination to getAll and searchBooks controllers

--- src/main/java/mate/academy/webapp/controller/BookController.java
+++ src/main/java/mate/academy/webapp/controller/BookController.java
@@ -3,10 +3,11 @@ package mate.academy.webapp.controller;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import mate.academy.webapp.dto.BookDto;
-import mate.academy.webapp.dto.BookSearchParametersDto;
-import mate.academy.webapp.dto.CreateBookRequestDto;
+import mate.academy.webapp.dto.book.BookDto;
+import mate.academy.webapp.dto.book.BookSearchParametersDto;
+import mate.academy.webapp.dto.book.CreateBookRequestDto;
 import mate.academy.webapp.service.BookService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +27,8 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.getAll();
+    public List<BookDto> getAll(Pageable pageable) {
+        return bookService.getAll(pageable);
     }
 
     @GetMapping("/{id}")
@@ -35,8 +37,8 @@ public class BookController {
     }
 
     @GetMapping("/search")
-    public List<BookDto> searchBooks(BookSearchParametersDto searchParameters) {
-        return bookService.searchBooks(searchParameters);
+    public List<BookDto> searchBooks(BookSearchParametersDto searchParameters, Pageable pageable) {
+        return bookService.searchBooks(searchParameters, pageable);
     }
 
     @PostMapping
@@ -50,8 +52,8 @@ public class BookController {
         return bookService.update(id, requestDto);
     }
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         bookService.delete(id);
     }
--- src/main/java/mate/academy/webapp/service/BookService.java
+++ src/main/java/mate/academy/webapp/service/BookService.java
@@ -1,20 +1,21 @@
 package mate.academy.webapp.service;
 
 import java.util.List;
-import mate.academy.webapp.dto.BookDto;
-import mate.academy.webapp.dto.BookSearchParametersDto;
-import mate.academy.webapp.dto.CreateBookRequestDto;
+import mate.academy.webapp.dto.book.BookDto;
+import mate.academy.webapp.dto.book.BookSearchParametersDto;
+import mate.academy.webapp.dto.book.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
     BookDto findById(Long id);
 
-    List<BookDto> getAll();
+    List<BookDto> getAll(Pageable pageable);
 
     BookDto update(Long id, CreateBookRequestDto requestDto);
 
     void delete(Long id);
 
-    List<BookDto> searchBooks(BookSearchParametersDto searchParameters);
+    List<BookDto> searchBooks(BookSearchParametersDto searchParameters, Pageable pageable);
 }
