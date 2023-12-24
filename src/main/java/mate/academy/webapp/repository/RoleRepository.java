334e09c463920bde7feeb9d3ea09e95a2631421a
Dima Semeniuk <semeniuk.dima@ukr.net>
2023-12-24 12:45:57 +0200

    added user role "USER" setting when saving to DB

--- src/main/java/mate/academy/webapp/repository/RoleRepository.java
+++ src/main/java/mate/academy/webapp/repository/RoleRepository.java
@@ -4,4 +4,5 @@ import mate.academy.webapp.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByRoleName(Role.RoleName roleName);
 }
--- src/main/java/mate/academy/webapp/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/webapp/service/impl/UserServiceImpl.java
@@ -5,7 +5,9 @@ import mate.academy.webapp.dto.user.UserRegistrationRequestDto;
 import mate.academy.webapp.dto.user.UserResponseDto;
 import mate.academy.webapp.exception.RegistrationException;
 import mate.academy.webapp.mapper.UserMapper;
+import mate.academy.webapp.model.Role;
 import mate.academy.webapp.model.User;
+import mate.academy.webapp.repository.RoleRepository;
 import mate.academy.webapp.repository.UserRepository;
 import mate.academy.webapp.service.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
 
@@ -23,8 +26,10 @@ public class UserServiceImpl implements UserService {
         if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
             throw new RegistrationException("Can't register user");
         }
-        requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
-        User savedUser = userRepository.save(userMapper.toModel(requestDto));
-        return userMapper.toDto(savedUser);
+        User user = userMapper.toModel(requestDto);
+        Role role = roleRepository.findByRoleName(Role.RoleName.USER);
+        user.getRoles().add(role);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userMapper.toDto(userRepository.save(user));
     }
 }
